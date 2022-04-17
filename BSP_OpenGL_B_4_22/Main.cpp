1f64a064b6762a428c012994eb92216b49cad849
Rakesh Jadav <rakeshcjadav@gmail.com>
2022-04-17 12:18:47 +0530

    compact profile

--- BSP_OpenGL_B_4_22/Main.cpp
+++ BSP_OpenGL_B_4_22/Main.cpp
@@ -23,31 +23,39 @@ int main()
 
     // TODO: Revisit once triangle is drawn
     //glViewport(0, 0, 800, 600);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Alpha Blend
+    //glBlendFunc(GL_ONE, GL_ZERO); // Replace Blend
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
     {
         // State Setting function
-        glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.3f, 0.2f, 1.0f);
         // State Using function
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_DEPTH_TEST);
+        // Red Quad
+        glBegin(GL_TRIANGLE_STRIP);
+            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+            
+            glVertex3f(0.0f, 0.5f, 0.10f);  // 0
+            glVertex3f(0.0f, 0.0f, 0.10f);  // 1
+            glVertex3f(0.5f, 0.5f, 0.10f);  // 3
+            glVertex3f(0.5f, 0.0f, 0.10f);  // 2
 
-        // State setting fucntion
-        glBegin(GL_TRIANGLES);
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glVertex3f(-0.5f, 0.1f, -0.05f);
-            glVertex3f(0.5f, 0.1f, -0.05f);
-            glVertex3f(0.0f, -0.5f, -0.05f);
         glEnd();
 
+        // Blue
         glBegin(GL_TRIANGLES);
-            glColor4f(1.0f, 0.0f, 0.0f, 0.1f);
-            glVertex3f(-0.5f, 0.0f, -0.10f);
-            glVertex3f(0.5f, 0.0f, -0.10f);
-            glVertex3f(0.0f, 0.5f, -0.10f);
+            glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+            
+            glVertex3f(-0.5f, 0.1f, 0.05f);
+            glVertex3f(0.0f, -0.5f, 0.05f);
+            glVertex3f(0.5f, 0.1f, 0.05f);
         glEnd();
 
         // Double buffering 
