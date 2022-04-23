84b97fb8d4870eb1e02ed49e6ee45e77778e2965
Rakesh Jadav <rakeshcjadav@gmail.com>
2022-04-23 13:30:15 +0530

    Added GLAD and Draw triangle using 3.3 Core profile (VAO, Shaders, Program)

--- BSP_OpenGL_B_4_22/Main.cpp
+++ BSP_OpenGL_B_4_22/Main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include"glad/glad.h"
 #include<GLFW\glfw3.h>
 
 using namespace std;
@@ -9,7 +10,7 @@ int main()
     // State setting function : GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -21,8 +22,14 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     // TODO: Revisit once triangle is drawn
-    //glViewport(0, 0, 800, 600);
+    glViewport(0, 0, 800, 600);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
 
@@ -31,6 +38,102 @@ int main()
     //glBlendFunc(GL_ONE, GL_ZERO); // Replace Blend
     glEnable(GL_DEPTH_TEST);
 
+    float vertices[] = {
+        // position             // color
+        -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,
+         0.0f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f
+    }; // float = 4 bytes means 4*9 = 36 bytes // 4*12 = 48 bytes
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    {
+        // Creating Vertex Buffer Object
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // location 0, read 3 GL_FLOAT and jump(stride) 3 * sizeof(float) and offset is 0
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(0 * sizeof(float)));
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        // location 2, read 3 GL_FLOAT and jump(stride) 4 * sizeof(float) and offset is 1
+        // glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(1 * sizeof(float)));
+        // glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    glBindVertexArray(0);
+
+
+    const char* VertexShaderSource = 
+        "#version 330 core\n"
+        "layout(location = 0) in vec3 aPos;\n"
+        "layout(location = 1) in vec3 Color;\n"
+
+        "out vec3 outColor;\n"
+
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "    outColor = Color;\n"
+        "}\n";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &VertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    const char* FragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+
+        "in vec3 outColor;\n"
+
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(outColor.r, outColor.g, outColor.b, 1.0f);\n"
+        "}\n";
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &FragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) 
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     while (!glfwWindowShouldClose(window))
     {
         // State Setting function
@@ -38,25 +141,11 @@ int main()
         // State Using function
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Red Quad
-        glBegin(GL_TRIANGLE_STRIP);
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-            
-            glVertex3f(0.0f, 0.5f, 0.10f);  // 0
-            glVertex3f(0.0f, 0.0f, 0.10f);  // 1
-            glVertex3f(0.5f, 0.5f, 0.10f);  // 3
-            glVertex3f(0.5f, 0.0f, 0.10f);  // 2
 
-        glEnd();
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // Blue
-        glBegin(GL_TRIANGLES);
-            glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
-            
-            glVertex3f(-0.5f, 0.1f, 0.05f);
-            glVertex3f(0.0f, -0.5f, 0.05f);
-            glVertex3f(0.5f, 0.1f, 0.05f);
-        glEnd();
 
         // Double buffering 
         glfwSwapBuffers(window);
@@ -65,4 +154,27 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+void RenderCompactProfile()
+{
+    // Red Quad
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.5f, 0.10f);  // 0
+    glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.10f);  // 1
+    glVertex3f(0.5f, 0.5f, 0.10f);  // 3
+    glVertex3f(0.5f, 0.0f, 0.10f);  // 2
+
+    glEnd();
+
+    // Blue
+    glBegin(GL_TRIANGLES);
+    glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+
+    glVertex3f(-0.5f, 0.1f, 0.05f);
+    glVertex3f(0.0f, -0.5f, 0.05f);
+    glVertex3f(0.5f, 0.1f, 0.05f);
+    glEnd();
 }
