fb7126010d54b5dc3f0bbaa525647bc9aeefac1d
Rakesh Jadav <rakeshcjadav@gmail.com>
2022-04-30 13:13:49 +0530

    Functions

--- BSP_OpenGL_B_4_22/Main.cpp
+++ BSP_OpenGL_B_4_22/Main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+unsigned int CreateMesh(float* vertices, int nVertices, unsigned int* indices, int nIndices);
+unsigned int CreateProgram(const char* VertexShaderSource, const char* FragmentShaderSource);
 void RenderCompactProfile();
 
 int main()
@@ -43,20 +45,121 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
     float vertices[] = {
-        // position             // color
-        -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,       // top left
-        -0.5f, 0.5f, 0.0f,      0.0f, 1.0f, 0.0f,       // bottom left
-         0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,       // bottom right
-         0.5f, 0.5f, 0.0f,      0.0f, 1.0f, 0.0f,       // top right
-
-         -0.5f, 1.0f, 0.0f,      0.0f, 1.0f, 0.0f,        // top top right
-          0.5f, 1.0f, 0.0f,      0.0f, 1.0f, 0.0f         // top top left
+        // position
+        -0.5f, -0.5f, 0.0f,       // top left
+        -0.5f, 0.5f, 0.0f,        // bottom left
+         0.5f, -0.5f, 0.0f,       // bottom right
+         0.5f, 0.5f, 0.0f,        // top right
     };
 
     unsigned int indices[] = {
+        0, 2, 1, 3
+    };
+
+    unsigned int mesh = CreateMesh(vertices, sizeof(vertices) / sizeof(float), indices, sizeof(indices) / sizeof(unsigned int));
+
+    float verticeRectangle[] = {
+        // position
+        -0.5f, -0.5f, 0.0f,       // top left
+        -0.5f, 0.5f, 0.0f,        // bottom left
+         0.5f, -0.5f, 0.0f,       // bottom right
+         0.5f, 0.5f, 0.0f,        // top right
+
+         -0.5f, 1.0f, 0.0f,       // top top right
+          0.5f, 1.0f, 0.0f,       // top top left
+    };
+
+    unsigned int indicesRectangle[] = {
         0, 2, 1, 3, 4, 5
     };
 
+    unsigned int meshRectangle = CreateMesh(vertices, sizeof(vertices) / sizeof(float), indices, sizeof(indices) / sizeof(unsigned int));
+
+    const char* VertexShaderSource = 
+        "#version 330 core\n"
+        "layout(location = 0) in vec3 aPosition;\n"
+
+        "uniform vec3 uOffset;\n"
+
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPosition + uOffset, 1.0);\n"
+        "}\n";
+
+    const char* VertexShaderSourceSomething =
+        "#version 330 core\n"
+        "layout(location = 0) in vec3 aPosition;\n"
+
+        "uniform vec3 uOffset;\n"
+
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPosition + uOffset.yxz, 1.0);\n"
+        "}\n";
+
+    const char* FragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+
+        "uniform vec3 uColor;\n"
+
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(uColor, 1.0f);\n"
+        "}\n";
+
+    unsigned int shaderProgram = CreateProgram(VertexShaderSource, FragmentShaderSource);
+    unsigned int shaderProgramSomething = CreateProgram(VertexShaderSourceSomething, FragmentShaderSource);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // State Setting function
+        glClearColor(0.0f, 0.3f, 0.2f, 1.0f);
+        // State Using function
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        float timeValue = (float)glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+
+        glUseProgram(shaderProgram);
+        {
+            int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
+            glUniform3f(colorLocation, 0.0f, greenValue, 1.0f);
+
+            int offsetLocation = glGetUniformLocation(shaderProgram, "uOffset");
+            glUniform3f(offsetLocation, -0.2f, 0.0f, 0.0f);
+        }
+        glBindVertexArray(mesh);
+        {
+            //glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);      // If you just VBOs
+            glDrawElements(GL_TRIANGLE_STRIP, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0); // If you have EBOs defined
+        }
+
+
+        glUseProgram(shaderProgramSomething);
+        {
+            int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
+            glUniform3f(colorLocation, 0.0f, 0.0f, greenValue);
+
+            int offsetLocation = glGetUniformLocation(shaderProgram, "uOffset");
+            glUniform3f(offsetLocation, 0.2f, 0.0f, 0.0f);
+        }
+        glBindVertexArray(meshRectangle);
+        {
+            glDrawElements(GL_TRIANGLE_STRIP, sizeof(indicesRectangle) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+        }
+
+        // Double buffering 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+unsigned int CreateMesh(float * vertices, int nVertices, unsigned int * indices, int nIndices)
+{
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -66,14 +169,14 @@ int main()
         unsigned int VBO;
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * nVertices, vertices, GL_STATIC_DRAW);
 
         // location 0, read 3 GL_FLOAT and jump(stride) 3 * sizeof(float) and offset is 0
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(0 * sizeof(float)));
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0 * sizeof(float)));
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
+        //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        //glEnableVertexAttribArray(1);
 
         // location 2, read 3 GL_FLOAT and jump(stride) 4 * sizeof(float) and offset is 1
         // glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(1 * sizeof(float)));
@@ -84,40 +187,16 @@ int main()
         unsigned int EBO;
         glGenBuffers(1, &EBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * nIndices, indices, GL_STATIC_DRAW);
         //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
     glBindVertexArray(0);
 
-    const char* VertexShaderSource = 
-        "#version 330 core\n"
-        "layout(location = 0) in vec3 aPosition;\n"
-        "layout(location = 1) in vec3 aColor;\n"
+    return VAO;
+}
 
-        "//out vec3 outColor;\n"
-
-        "uniform float uSomething;\n"
-
-        "//uniform vec3 uColor;\n"
-
-        "uniform vec3 uColors[3];\n"
-
-        "float Offset = 0.2;\n"
-        "vec3 vOffset = vec3(0.2, 0.0, 0.2);\n"
-
-        "vec3 vScale = vec3(2.0, 2.0, 0.0);\n"
-
-        "void main()\n"
-        "{\n"
-        "    vec3 newPos = aPosition * vec3(uSomething) + vec3(uSomething, 0.0, 0.0);\n"
-        "    gl_Position = vec4(newPos.xyz, 1.0);\n"
-        "    //outColor = uColor;\n"
-        "}\n";
-
-    //=> vecn => .x, .y, .z, .w => Position
-    //=> vecn => .r, .g, .b, .a => Color
-    //=> vecn => .s, .t, .p, .q => Texture Coordinate
-
+unsigned int CreateVertexShader(const char* VertexShaderSource)
+{
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &VertexShaderSource, NULL);
@@ -130,38 +209,40 @@ int main()
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+    return vertexShader;
+}
 
-    const char* FragmentShaderSource =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-
-        "//in vec3 outColor;\n"
-
-        "uniform vec3 uColor;\n"
-
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(uColor, 1.0f);\n"
-        "}\n";
-
+unsigned int CreateFragmentShader(const char* FragmentShaderSource)
+{
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &FragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+    int  success;
+    char infoLog[512];
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+    return fragmentShader;
+}
+
+unsigned int CreateProgram(const char* VertexShaderSource, const char* FragmentShaderSource)
+{
+    unsigned int vertexShader = CreateVertexShader(VertexShaderSource);
+    unsigned int fragmentShader = CreateFragmentShader(FragmentShaderSource);
 
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+    int  success;
+    char infoLog[512];
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) 
+    if (!success)
     {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
@@ -170,47 +251,7 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glUseProgram(shaderProgram);
-
-    int somethingLocation = glGetUniformLocation(shaderProgram, "uSomething");
-    glUniform1f(somethingLocation, 0.0f);
-
-    int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
-    glUniform3f(colorLocation, 0.0f, 0.0f, 1.0f);
-
-
-    while (!glfwWindowShouldClose(window))
-    {
-        // State Setting function
-        glClearColor(0.0f, 0.3f, 0.2f, 1.0f);
-        // State Using function
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-
-        glUseProgram(shaderProgram);
-        //int colorLocation = glGetUniformLocation(shaderProgram, "uColor");
-        glUniform3f(colorLocation, 0.0f, greenValue, 1.0f);
-
-        //float color[9] = {0.0f, greenValue, 1.0f, 0.0f, greenValue, 1.0f , 0.0f, greenValue, 1.0f };
-        //glUniform3fv(colorLocation, 3, color);
-
-        //int somethingLocation = glGetUniformLocation(shaderProgram, "uSomething");
-        glUniform1f(somethingLocation, greenValue);
-
-        glBindVertexArray(VAO);
-
-        //glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);      // If you just VBOs
-        glDrawElements(GL_TRIANGLE_STRIP, sizeof(indices)/sizeof(unsigned int), GL_UNSIGNED_INT, 0); // If you have EBOs defined
-
-        // Double buffering 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+    return shaderProgram;
 }
 
 void RenderCompactProfile()
